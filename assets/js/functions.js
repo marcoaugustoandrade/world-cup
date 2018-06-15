1a5a379424fcbcc43d205b70f4437674292f6e44
Marco Antonio Augusto de Andrade <marco.andrade@ifro.edu.br>
2018-06-15 09:36:30 -0400

    ..

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,9 +8,84 @@ function carregarDados(){
     
     request.addEventListener("load", function(){
         if (request.status == 200){
+            
             dados = JSON.parse(request.responseText);
             console.log("Carregando dados...");
-            console.log(dados); 
+            
+            dados.confrontos.forEach(function(elemento){
+                //console.log(elemento);
+                
+                let container = document.querySelector("#confrontos");
+                
+                let jogoContainer = document.createElement("div");
+                jogoContainer.classList.add("jogo-container");
+                jogoContainer.classList.add("egi-uru");
+                container.appendChild(jogoContainer);
+
+                    let jogoContainerConteudo = document.createElement("div");
+                    jogoContainerConteudo.classList.add("jogo-container--conteudo");
+                    jogoContainer.appendChild(jogoContainerConteudo);
+
+                        let jogoContainerConteudo1 = document.createElement("div");
+                        jogoContainerConteudo.appendChild(jogoContainerConteudo1);
+
+                            let pais1 = document.createElement("div");
+                            pais1.classList.add("pais");
+                            jogoContainerConteudo1.appendChild(pais1);
+
+                                let img1 = document.createElement("img");
+                                img1.classList.add("pais--flag");
+                                img1.src = "assets/images/"+elemento.time_1_flag;
+                                pais1.appendChild(img1);
+                                let paisSigla1 = document.createElement("span");
+                                paisSigla1.classList.add("pais--sigla");
+                                paisSigla1.textContent = elemento.time_1_sigla;
+                                pais1.appendChild(paisSigla1);
+
+
+                        let jogoContainerConteudo2 = document.createElement("div");
+                        jogoContainerConteudo.appendChild(jogoContainerConteudo2);
+
+                            let resultado = document.createElement("div");
+                            resultado.classList.add("resultado");
+                            jogoContainerConteudo2.appendChild(resultado);
+
+                                let resultadoSpan1 = document.createElement("span");
+                                resultadoSpan1.textContent = elemento.time_1_gols;
+                                resultado.appendChild(resultadoSpan1);
+                                let resultadoSpan2 = document.createElement("span");
+                                resultadoSpan2.textContent = "x";
+                                resultado.appendChild(resultadoSpan2);
+                                let resultadoSpan3 = document.createElement("span");
+                                resultadoSpan3.textContent = elemento.time_2_gols;
+                                resultado.appendChild(resultadoSpan3);
+                        
+                        let jogoContainerConteudo3 = document.createElement("div");
+                        jogoContainerConteudo.appendChild(jogoContainerConteudo3);
+
+                            let pais2 = document.createElement("div");
+                            pais2.classList.add("pais");
+                            jogoContainerConteudo3.appendChild(pais2);
+
+                                let paisSigla2 = document.createElement("span");
+                                paisSigla2.classList.add("pais--sigla");
+                                paisSigla2.textContent = elemento.time_2_sigla;
+                                pais2.appendChild(paisSigla2);
+                                let img2 = document.createElement("img");
+                                img2.classList.add("pais--flag");
+                                img2.src = "assets/images/"+elemento.time_2_flag;
+                                pais2.appendChild(img2);
+                    
+                    let jogoContainerHorario = document.createElement("div");
+                    jogoContainerHorario.classList.add("jogo-container--horario");
+                    jogoContainer.appendChild(jogoContainerHorario);
+
+                        let jogoContainerHorarioTexto = document.createElement("span");
+                        jogoContainerHorarioTexto.textContent = "09h00";
+                        jogoContainerHorario.appendChild(jogoContainerHorarioTexto);
+
+            });
+
         } else {
             console.log("Erro ao carrega dados do servidor:" + request.responseText);
         }
